2932fc57b0c0bc34660d74568d4312b8a49dc929
Audrick pinson <audrick.pinson@gmail.com>
2023-05-16 18:29:13 +0200

    modif fichier auth, integration du fichier avec modif require transmition info gestionnaire route

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const auth = require('auth');
 const router = express.Router();
 
 const userCtrl = require('../controllers/users');
 
-router.post('/signup', userCtrl.signup);
-router.post('/login', userCtrl.login);
+router.post('/signup', auth, userCtrl.signup);
+router.post('/login', auth, userCtrl.login);
 
 module.exports = router;
